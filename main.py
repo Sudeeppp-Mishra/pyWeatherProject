1891dbdccc9ea7c064035f5f0bd102804d495402
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-06 19:06:10 +0545

    feat: add basic widgets (Label, PushButton, LineEdit) for WeatherApp

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtCore import Qt
+
+# Load environment variables from .env
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("70℉(placeholder)", self)
+        self.emoji_label = QLabel("☀️")
+        self.description_label = QLabel("Sunny", self)
+        
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
