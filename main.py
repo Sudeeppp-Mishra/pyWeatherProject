c4bb2940145c852e213abb735a3bcf7ef56d169e
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-08 06:07:56 +0545

    feat: fix the font size of temperature_label to 75px
    If we had some error then the tempearute_label shows font-size of 30px as we have fixed it for error but after getting error if we get our city correct and get weather info then the temperature label will show so small font so we fixed it to 75px here.

--- main.py
+++ main.py
@@ -137,6 +137,7 @@ class WeatherApp(QWidget):
         self.temperature_label.setText(message)
     
     def display_weather(self, data):
+        self.temperature_label.setStyleSheet("font-size: 75px;")
         temperature_K = data["main"]["temp"]
         temperature_C = temperature_K - 273.15
         temperature_F = (temperature_K*9/5)-459.67
