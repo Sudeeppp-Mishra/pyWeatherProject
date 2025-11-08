bff54d7c610bf0bb62154fcc27bbe5a91b010ffe
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-11-08 06:37:34 +0545

    feat: implement get_weather_emoji() method according to the weather_id

--- main.py
+++ main.py
@@ -152,7 +152,28 @@ class WeatherApp(QWidget):
         
     @staticmethod   
     def get_weather_emoji(weather_id):
-        pass
+        if 200<=weather_id<=232:
+            return "⛈️"
+        elif 300<=weather_id<=321:
+            return "🌦️"
+        elif 500<=weather_id<=531:
+            return "🌧️"
+        elif 600<=weather_id<=622:
+            return "❄️"
+        elif 701<=weather_id<=741:
+            return "😶‍🌫️"
+        elif weather_id == 762:
+            return "🌋"
+        elif weather_id == 771:
+            return "💨"
+        elif weather_id==781:
+            return "🌪️"
+        elif weather_id==800:
+            return "☀️"
+        elif 801<=weather_id<=804:
+            return "☁️"
+        else:
+            return ""
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
